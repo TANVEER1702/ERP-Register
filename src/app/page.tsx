f6bf986531b9a90fad078646b295ac6545fa80cf
TANVEER1702 <tanveerali88ali@gmail.com>
2025-03-03 16:06:36 +0530

    main page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
  <>
-<h1 className="flex justify-center text-center items-center text-4xl font-bold h-screen">This is main page</h1>
+<div className="flex justify-around font-bold text-2xl items-center text-center mt-8">
+<Link href={"./login"}>Login</Link>
+<Link href={"./Register"}>Register</Link>
+</div>
+ <div className="flex justify-center text-center items-center mt-32">
+<h1 className=" text-4xl font-bold">This is main page</h1>
+ </div>
+ <footer className="text-center p-4 bg-gray-200 text-gray-700 mt-56">
+      Developed by <span className="font-bold">Tanveer Ali</span>
+    </footer>
+
  </>
   );
 }
